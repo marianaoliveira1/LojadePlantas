8a04c2774b117db12650fa2ff1b19c3354bce492
Mariana Oliveira <marianaoliveirafernandes19@gmail.com>
2021-10-26 12:46:08 -0300

    alterações

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,90 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class ShopScreen extends StatelessWidget {
+class ShopScreen extends StatefulWidget {
+  const ShopScreen({Key? key}) : super(key: key);
+
   @override
   _ShopScreenState createState() => _ShopScreenState();
 }
 
-class _ShopScreenState extends State<ShopScreen>
-    with SingleTickerProviderStateMixin {}
+class _ShopScreenState extends State<ShopScreen> 
+    with SingleTickerProviderStateMixin {
 
+  TabController? _tabController;
+  
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _tabController = TabController(initialIndex: 0, length: 5, vsync: this);
+  }
 
-
-// class ShopScreen extends StatelessWidget {
-
-//   @override
-//   Widget build(BuildContext context)  {
-//     return Scaffold(
-//         backgroundColor: Colors.white,
-//         body: AnnotatedRegion<SystemUiOverlayStyle>(
-//           value: SystemUiOverlayStyle.dark,
-//           child: ListView(
-//             padding: EdgeInsets.symmetric(vertical: 60.0),
-//             children: [
-//               Padding(
-//                 padding: EdgeInsets.symmetric(horizontal: 30.0),
-//                 child: Row(
-//                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                   children: [
-//                     Icon(
-//                       Icons.menu,
-//                       size: 30.0,
-//                       color: Colors.grey,
-//                     ),
-//                     Icon(
-//                       Icons.shopping_cart,
-//                       size: 30.0,
-//                       color: Colors.grey,
-//                     ),
-//                   ],
-//                 ),
-//               ),
-//               SizedBox(height: 20.0),
-//               Padding(
-//                 padding: EdgeInsets.only(left: 30.0),
-//                 child: Text(
-//                   'Melhores escolhas',
-//                   style: TextStyle(
-//                     fontSize: 24.0,
-//                     fontWeight: FontWeight.w600,
-//                   ),
-//                 ),
-//               ),
-//               SizedBox(height: 20.0)
-//               TabBar(),
-//             ],
-//           ),
-//         ),
-//       );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.dark,
+        child: ListView(
+          padding: EdgeInsets.symmetric(vertical: 60.0),
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(
+                    Icons.menu,
+                    size: 30.0,
+                    color: Colors.grey
+                  ),
+                  Icon(
+                      Icons.shopping_cart,
+                      size: 30.0,
+                      color: Colors.black
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 30.0),
+              child: Text(
+                'Melhores Escolhas',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            TabBar(
+              controller: _tabController,
+              indicatorColor: Colors.transparent,
+              labelColor: Colors.black,
+              unselectedLabelColor: Colors.grey.withOpacity(0.6),
+              labelPadding: EdgeInsets.symmetric(horizontal: 35.0),
+              isScrollable: true,
+              tabs: [
+                Tab(
+                  child: Text(
+                    'Top',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
