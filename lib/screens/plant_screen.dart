77a0fca0dffc599ae839d11a9de1c6a5ebe8751c
Mariana Oliveira <marianaoliveirafernandes19@gmail.com>
2021-10-30 12:01:34 -0300

    comprar as plantas

--- lib/screens/plant_screen.dart
+++ lib/screens/plant_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:flutter/services.dart';
 import 'package:planta/models/plant.dart';
 
@@ -37,10 +39,13 @@ class _PlantScreenState extends State<PlantScreen> {
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Icon(
-                              Icons.arrow_back,
-                              size: 30.0,
-                              color: Colors.white
+                          GestureDetector(
+                            onTap: () => Navigator.pop(context),
+                            child: Icon(
+                                Icons.arrow_back,
+                                size: 30.0,
+                                color: Colors.white
+                            ),
                           ),
                           Icon(
                               Icons.shopping_cart,
@@ -48,11 +53,124 @@ class _PlantScreenState extends State<PlantScreen> {
                               color: Colors.black
                           ),
                         ],
-                      )
+                      ),
+                      SizedBox(height: 20.0,),
+                      Text(
+                        widget.plant.category.toUpperCase(),
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15.0,
+                        ),
+                      ),
+                      SizedBox(height: 5.0,),
+                      Text(
+                        widget.plant.name,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 40.0,),
+                      Text(
+                        'De',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15.0,
+                        ),
+                      ),
+                      SizedBox(height: 5.0,),
+                      Text(
+                        '\$${widget.plant.price}',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 40.0,),
+                      Text(
+                        'Sizw',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15.0,
+                        ),
+                      ),
+                      SizedBox(height: 5.0,),
+                      Text(
+                        widget.plant.size,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 40.0,),
+                      RawMaterialButton(
+                        padding: EdgeInsets.all(20.0),
+                        shape: CircleBorder(),
+                        elevation: 2.0,
+                        fillColor: Colors.black,
+                        child: Icon(
+                          Icons.add_shopping_cart,
+                          color: Colors.white,
+                          size: 35.0,
+                        ),
+                        onPressed: () => print('Add ao carrinho'),
+                      ),
                     ],
                   ),
                 ),
+                Positioned(
+                  right: 20.0,
+                  bottom: 30.0,
+                  child: Hero(
+                    tag: widget.plant.imageUrl,
+                    child: Image(
+                      height: 280.0,
+                      width: 280.0,
+                      image: AssetImage(widget.plant.imageUrl),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
               ],),
+              Container(
+                height: 400.0,
+                transform: Matrix4.translationValues(0.0, -20.0, 0.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(
+                        left: 30.0,
+                        right: 30.0,
+                        top: 40.0,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'All to know',
+                            style: TextStyle(
+                              fontSize: 24.0,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          SizedBox(height: 10.0,),
+                          Text(
+                            widget.plant.description,
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
